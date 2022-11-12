e11a8c7248b772d0224c75c5a72e5653465b9467
Matheus <matheusrcrv07@gmail.com>
2022-11-12 13:54:49 -0300

    Testes tela Inicial e NotFound completo

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,6 @@
 import image from '../../assets/error-404.png'
 import { NotFoundContainer } from './NotFound.styled'
 import { BiArrowBack } from 'react-icons/bi'
-import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
@@ -10,7 +9,7 @@ const NotFound = () => {
         <div>
           <h1>Página não encontrada</h1>
           <img src={image} alt="404 Github" width={350} />
-          <Link to={'/'}><button><i><BiArrowBack /></i></button></Link>
+          <a href='/'><button><i><BiArrowBack /></i></button></a>
         </div>
       </NotFoundContainer>
     </>
