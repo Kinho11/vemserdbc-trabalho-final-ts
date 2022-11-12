47b858996d53bffb508e3ea4e37c71cd64ac681d
Matheus <matheusrcrv07@gmail.com>
2022-11-11 22:24:42 -0300

    add: Adicionada tela Home

--- src/pages/Inicial/Inicial.tsx
+++ src/pages/Inicial/Inicial.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import { InicialContainer } from './Inicial.styled'
+import { FiSend } from 'react-icons/fi'
+import { useState } from 'react'
 
 export const Inicial = () => {
+  const [name, setName] = useState<string>('')
+
   return (
-    <div>Inicial</div>
+    <>
+      <InicialContainer>
+        <div className='infos'>
+          <h1>Gitstats</h1>
+          <p>Digite seu nome de usuário e veja informações sobre seu perfil</p>
+          <div className='input'>
+            <input type="text" placeholder='Username' onChange={(e) => setName(e.target.value)} />
+            <Link to={`/perfil/${name}`}><button type='submit'><i><FiSend size={28} /></i></button></Link>
+          </div>
+        </div>
+      </InicialContainer>
+    </>
   )
 }
