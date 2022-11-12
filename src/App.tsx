ed1e3511fc0ddd6e7255aaa949b8303bbd31bcb3
Matheus <matheusrcrv07@gmail.com>
2022-11-11 22:26:57 -0300

    Alteração nas rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import { Inicial } from './pages/Inicial/Inicial';
 import { Profile } from './pages/Profile/Profile';
@@ -10,8 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Inicial/>}/>
-          <Route path='/Profile' element={<Profile/>}/>
-
+          <Route path='/perfil/:username' element={<Profile/>}/>
         </Routes>
       </BrowserRouter>
     </>
